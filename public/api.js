cf38a47f954e17c3b736f25465c0ab7469725940
zhim57 <65269162+zhim57@users.noreply.github.com>
2023-01-30 16:30:21 -0500

    some changes

--- public/api.js
+++ public/api.js
@@ -1,20 +1,13 @@
-
-
-  import  { processImeiActual} from "/funk.js";
+import { processImeiActual } from "/funk.js";
 export const API = {
-  
-
-
-
   async getLastImei() {
     let res;
     try {
       res = await fetch("/api/imeis");
       // console.log("res: "+ JSON.stringify(res));
       // console.log("res: "+ JSON.stringify(res));
-      
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
     const json = await res.json();
     // console.log("res: "+ JSON.stringify(res));
@@ -25,74 +18,65 @@ export const API = {
     let res;
 
     try {
-      res = await fetch("/result1/" + imei,);
-      
+      res = await fetch("/result1/" + imei);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
     const json = await res.json();
     result(JSON.stringify(json));
- 
+
     return json[json.length - 1];
   },
-  async addRequest(data,id1) {
+  async addRequest(data, id1) {
     if (location.search.split("=")[1] === undefined) {
-     let id= id1;
-     const res = await fetch("/api/imeis/" + id, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data)
-    });
-
-    const json = await res.json();
-    
-
-    return json;
-      
-    }
-    else  {
-      let id2 = location.search.split("=")[1];
-      console.log(id2)
-   
-      const res = await fetch("/api/imeis/" + id2, {
+      let id = id1;
+      const res = await fetch("/api/imeis/" + id, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
       });
 
       const json = await res.json();
 
-
-  
       return json;
+    } else {
+      let id2 = location.search.split("=")[1];
+      console.log(id2);
 
+      const res = await fetch("/api/imeis/" + id2, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      const json = await res.json();
+
+      return json;
     }
-
-   
   },
-  async addRequests(data,id1) {
+  async addRequests(data, id1) {
     // const id = location.search.split("=")[1];
-   let id = id1;
-// console.log(data);
-console.log("add requests");
-// console.log(id);
+    let id = id1;
+    // console.log(data);
+    console.log("add requests");
+    // console.log(id);
     const res = await fetch("/api/imeis/" + id, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     });
 
     const json = await res.json();
 
     return json;
   },
-  async createImei(data={}) {
+  async createImei(data = {}) {
     // console.log("idiot0");
     console.log("create imei");
     const res = await fetch("/api/requests1", {
       method: "POST",
       body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" }
+      headers: { "Content-Type": "application/json" },
     });
 
     const json = await res.json();
